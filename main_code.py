e42a0f4e64dc47081adbe2cf15532f143cf7d3f0
Jon <jambler24@gmail.com>
2020-11-24 17:43:32 +0200

    Got curves in, added as function

--- main_code.py
+++ main_code.py
@@ -2,7 +2,7 @@ import argparse
 import itertools
 import pickle
 from testing_code import *
-
+from new_functions import *
 
 def main(in_data):
 
@@ -10,9 +10,28 @@ def main(in_data):
 
     test_data = pickle.load(infile)
 
+    print(in_data)
+
     print(test_data)
 
     plot_fig = ManhattanPlot(test_data, gene=None)
+
+
+    point_1 = {'x': 1081000000, 'y': 8.761}
+    point_2 = {'x': 1771000000, 'y': 8.737}
+
+    link_type = "PPI"
+
+    add_line_between_points(plot_fig, point_1, point_2, "Blue")
+
+    '''
+    
+    plot_fig.add_shape(
+        type="path",
+        path="M 1081000000,8.761 Q 1400000000,10 1771000000,8.737",
+        hoverinfo="SOMETHING"
+    )
+    '''
 
     plot_fig.show()
 
--- new_functions.py
+++ new_functions.py
@@ -1,15 +1,31 @@
 
-print('ok')
 
-# Add shapes
-fig.update_layout(
-    shapes=[
-        # Quadratic Bezier Curves
-        dict(
-            type="path",
-            path="M 4,4 Q 6,8 8,4",
-            line_color="RoyalBlue",
-        ),
 
-    ]
-)
+
+def add_line_between_points(fig_object, point_1, point_2, colour):
+
+
+    middle_point = int((point_1['x'] + point_2['x']) / 2)
+    peak_point = int(((point_1['y'] + point_2['y']) / 2) + 4)
+
+
+    path_string = "M " + str(point_1['x']) \
+                  + "," \
+                  + str(point_1['y']) \
+                  + " Q " \
+                  + str(middle_point) \
+                  + "," \
+                  + str(peak_point) \
+                  + " " \
+                  + str(point_2['x']) \
+                  + "," \
+                  + str(point_2['y'])
+
+    print(path_string)
+
+    fig_object.add_shape(
+        type="path",
+        path=path_string,
+    )
+
+
